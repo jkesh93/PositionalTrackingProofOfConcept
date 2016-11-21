b7c97a0dbeedd106fd830a355e4cf7e81deabb7d
jkesh93 <jay111893@gmail.com>
2016-11-21 00:47:34 -0500

    using the lowpassFilter variable, values can be updated only if they are different beyond a minimum thresh hold

--- app/src/main/java/com/example/jay/positionaltrackingproofofconcept/MainActivity.java
+++ app/src/main/java/com/example/jay/positionaltrackingproofofconcept/MainActivity.java
@@ -15,15 +15,66 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private float xAccel, yAccel, zAccel = 0.0f;
+    // low pass filter variables
+    private float lowPassFilter = .1f;
+    private float lpX, lpY,lpZ = 0.0f;
+    private float fX, fY, fZ = 0.0f;
+    private boolean firstpass = true;
     private SensorManager sensorManager;
     private SensorEventListener sensorEventListener = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent event) {
             if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+
+
+                // set x-Accel to the sensor value;
                 xAccel = event.values[0];
+                // if it's not the first pass -- compare the last value to the current value and if it's greater than the
+                // lowpassFilter thresh hold, then pass that value to fX (filtered value of X);
+                if(!firstpass){
+                    float diff = Math.abs(xAccel - lpX);
+                    if(diff > lowPassFilter){
+                        Log.v("DiffX", "" + diff);
+                        fX = xAccel;
+                    }
+                }
+
+                // value to compare the next sensor reading to;
+                lpX = xAccel;
+
+                // set y-Accel to the sensor value;
                 yAccel = -event.values[1];
+                // if it's not the first pass -- compare the last value to the current value and if it's greater than the
+                // lowpassFilter thresh hold, then pass that value to fY (filtered value of Y);
+                if(!firstpass){
+                    float diff = Math.abs(yAccel - lpY);
+                    if(diff > lowPassFilter){
+                        Log.v("DiffY", "" + diff);
+                        fY = yAccel;
+                    }
+                }
+
+                // value to compare the next sensor reading to;
+                lpY = yAccel;
+
+                // set z-Accel to the sensor value
                 zAccel = event.values[2];
+                // if it's not the first pass -- compare the last value to the current value and if it's greater than the
+                // lowpassFilter thresh hold, then pass that value to fZ (filtered value of Z);
+                if(!firstpass){
+                    float diff = Math.abs(zAccel - lpZ);
+                    if(diff > lowPassFilter){
+                        Log.v("DiffZ", "" + diff);
+                        fZ = zAccel;
+                    }
+                }
+
+                // value to compare the next sensor reading to;
+                lpZ = zAccel;
             }
+
+            // after the firstpass this is set to false to enable sensor-reading comparisons to take place
+            firstpass = false;
         }
 
         @Override
@@ -67,8 +118,10 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onDraw(Canvas canvas){
-            canvas.drawText("xAccel: " + xAccel, 100, 100, infoPaint);
-            canvas.drawText("yAccel: " + yAccel, 100, 150, infoPaint);
+           // canvas.drawText("xAccel: " + xAccel, 100, 100, infoPaint);
+            canvas.drawText("xAccel: " + fX, 100, 100, infoPaint);
+            //canvas.drawText("yAccel: " + yAccel, 100, 150, infoPaint);
+            canvas.drawText("yAccel: " + fY, 100, 150, infoPaint);
             canvas.drawText("zAccel: " + zAccel, 100, 200, infoPaint);
             invalidate();
         }
